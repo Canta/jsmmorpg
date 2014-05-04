0072d5182356d99fb813af2d8d73dc780ee61ca9
Daniel Cantarín <omega_canta@yahoo.com>
2014-05-03 22:57:18 -0300

    Added basic movement logic to moveable objects.

--- server/class/GameWorldObjects.js
+++ server/class/GameWorldObjects.js
@@ -3,15 +3,11 @@ var utils = require("./Utils");
 require("./GeoStuff");
 
 function GameWorldObject(){
-	this.width = 0;
-	this.height= 0;
-	this.depth = 0;
-	this.weight= 0;
-	this.coords= {
-		x: 0,
-		y: 0,
-		z: 0
-	};
+	this.width    = 0;
+	this.height   = 0;
+	this.depth    = 0;
+	this.weight   = 0;
+	this.position = new GeoStuff.Point3D(0,0,0);
 	
 	this.init = function(specs){
 		for (s in specs){
@@ -36,16 +32,47 @@ function StaticObject(){
 	return SolidObject.extend(this);
 }
 
-function MobileObject(){ 
+function MobileObject(){
+	
+	this.__timers = {"moving" : null};
+	this.__destination = null;
+	
+	this.is_moving = function(){
+		return !(this.__timers.moving === null);
+	}
+	
 	this.move = function(to){
-		if (to instanceof Array || to instanceof Object){
-			this.coords.x = (to[x]) ? to[x] : this.coords.x;
-			this.coords.y = (to[y]) ? to[y] : this.coords.y;
-			this.coords.z = (to[z]) ? to[z] : this.coords.z;
+		if (to instanceof GeoStuff.Point3D){
+			this.__destination = to;
+			
+			var tmp = function(){
+				if (this.position.in_range( this.__destination ) ){
+					this.stop();
+				} else {
+					this.__move(5);
+				}
+			}
+			
+			this.__timers.moving = setTimeout(tmp,100);
+			
+		} else {
+			throw new Error("GameWorldObjects.MobileObject.move: Point3D expected.");
 		}
 	}
 	
-	return StaticObject.extend(this);
+	this.__move = function(step){
+		this.position.x += step;
+		this.position.y += step;
+		this.position.z += step;
+	}
+	
+	this.stop = function(){
+		clearInterval(this.__timers.moving);
+		this.__timers.moving = null;
+	}
+	
+	
+	return SolidObject.extend(this);
 }
 
 
