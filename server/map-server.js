e94bf2aa2798f070d8d9059bcf5aad8bd315c5d8
Daniel Cantarín <omega_canta@yahoo.com>
2014-05-11 00:26:12 -0300

    Now the map server loads the maps, creates octrees for each one of them, and uses the default and its properties to give random coordinates to the client.

--- server/map-server.js
+++ server/map-server.js
@@ -4,10 +4,24 @@
  * 2) Load status from the world (db persisted).
  * 3) Startin up socket services.
  */
+var map_server = {};
+
 console.log("Loading classes...");
 var CLASSES = require("./class");
+
+map_server.octrees = [];
+
 console.log("Loading assets...");
 var DATA = require("./data");
+
+for (var i in DATA.maps){
+	if (DATA.maps[i] instanceof CLASSES.Map){
+		var o = new CLASSES.GeoStuff.Octree( DATA.maps[i] );
+		map_server.octrees.push(o);
+		map_server.octrees[map_server.octrees.length-1].map.start();
+	}
+}
+
 console.log("Loading world status...");
 //TODO: add some world status handling.
 
@@ -21,9 +35,16 @@ wss.on('connection', function(ws) {
 		
 	});
 	
-	var x = Math.floor(Math.random() * 1000);
-	var y = Math.floor(Math.random() * 1000);
-	ws.send("{\"current_map\": \"DemoMap\", \"current_coords\":{\"x\":"+x+",\"y\":"+y+"}}");
+	var x = Math.floor(Math.random() * map_server.octrees[0].map.width);
+	var y = Math.floor(Math.random() * map_server.octrees[0].map.height);
+	var z = Math.floor(Math.random() * map_server.octrees[0].map.depth);
+	
+	var start = {
+		current_map    : map_server.octrees[0].map.name,
+		current_coords : {x: x, y: y, z: z}
+	};
+	
+	ws.send( JSON.stringify( start ) );
 });
 
 console.log("Server running.");
