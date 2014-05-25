88c62cb295d3883ef4fc58c344fa54848b233778
Daniel Cantarín <omega_canta@yahoo.com>
2014-05-25 20:38:11 -0300

    Added map boundaries and more expanded spheres.

--- server/data/maps/demo.js
+++ server/data/maps/demo.js
@@ -17,13 +17,19 @@ module.exports = DemoMap = Map.extend({
 		world.clear();
 		var ground  = new OIMO.Body({size:[this.width, 40, this.depth], pos:[0,-20,0], world:world});
 		
+		var pared1  = new OIMO.Body({type:"box", size:[this.width, this.height, 40], pos:[0,0,-20], world:world});
+		var pared2  = new OIMO.Body({type:"box", size:[this.width, this.height, 40], pos:[0,0,this.depth -20], world:world});
+		var pared3  = new OIMO.Body({type:"box", size:[this.width, this.height, 40], pos:[this.width ,0,-20], world:world});
+		var pared4  = new OIMO.Body({type:"box", size:[this.width, this.height, 40], pos:[this.width,0,this.depth -20], world:world});
+		
 		for (var i = 0; i < 50; i++){
-			var bola    = new OIMO.Body({type:'sphere', size:[10*0.5], pos:[Math.random() * 100,Math.random() * 100,Math.random() * 100], move:true, world:world});
+			var bola    = new OIMO.Body({type:'sphere', size:[10*0.5], pos:[Math.random() * 10000,Math.random() * 10000,Math.random() * 10000], move:true, world:world});
 			this.mobiles.push(bola);
 		}
 		
-		this.statics.push(ground);
+		this.statics.push(ground, pared1, pared2, pared3, pared4);
 		
 		this._super();
+		
 	}
 });
